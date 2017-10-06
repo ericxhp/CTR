896fb54a9698a2b3dba9c8e6a625b15d6a1099ae
ericxu <ericxu@micron.com>
2017-10-06 12:18:29 +0800

    Add print env list

--- TestCase_1.py
+++ TestCase_1.py
@@ -1,8 +1,14 @@
+import os
 
 class TestCase_1(object):
 
 	def show(self):
 		print "This Test case 1"
+		print "env list"
+		env_dist = os.environ
+		for key in env_dist:
+			print key + ' : ' + env_dist[key]
+		
 
 
 
--- TestCase_2.py
+++ TestCase_2.py
@@ -1,8 +1,12 @@
-
+import os
 class TestCase_2(object):
 
 	def show(self):
 		print "This Test case 2"
+		print "env list"
+		env_dist = os.environ
+		for key in env_dist:
+			print key + ' : ' + env_dist[key]
 
 
 
